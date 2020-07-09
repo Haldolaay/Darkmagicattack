e885c44fd0864ff2576fbd047b4944cb0f6000e0
hamzah <haldolaay@gmail.com>
2020-07-08 21:24:14 -0700

    hello darkness

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/header'
-import {BrowserRouter as Router} from 'react-router-dom'
-import {Route} from 'react-router-dom'
-import Rout from './components/route'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/home/header";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Rout from "./components/route/route";
 function App() {
   return (
-  
     <div className="App">
-      <Rout/>
+      <Rout />
     </div>
-   
   );
 }
 
